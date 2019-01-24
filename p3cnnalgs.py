3dedadbec9392e68372d917bb229235219be294f
owenzhong <owenzhong@ponddy-edu.com>
2019-01-24 17:51:24 +0800

    Add some alg

--- p3cnnalgs.py
+++ p3cnnalgs.py
@@ -31,10 +31,10 @@ def build_model():
     model.add(Dense(tone_num, activation='softmax'))
     return model
 
-def build_model(img_w, img_h, tone_num):
+def build_model(img_h, img_w, tone_num):
     model = Sequential()
 
-    model.add(Conv2D(32, kernel_size=(3, 3), input_shape=(img_w, img_h, 1),
+    model.add(Conv2D(32, kernel_size=(3, 3), input_shape=(img_h, img_w, 1),
                 activation='relu', padding='same'))
     model.add(MaxPool2D())
     model.add(Dropout(0.3))
@@ -61,8 +61,8 @@ def build_model(img_w, img_h, tone_num):
 
 
 # -------------------VGG16{S}-------------------
-def build_VGG16(img_w, img_h, nb_classes):
-    INPUTSIZE = (img_w, img_h, 1)
+def build_VGG16(img_h, img_w, nb_classes, channels=3):
+    INPUTSIZE = (img_h, img_w, channels)
     
     model = Sequential()  
     model.add(Conv2D(64,(3,3),strides=(1,1),input_shape=INPUTSIZE,padding='same',activation='relu',kernel_initializer='random_normal'))  
@@ -99,9 +99,9 @@ def build_VGG16(img_w, img_h, nb_classes):
 
 
 # -------------------VGG19{S}-------------------
-def build_VGG19(img_w, img_h, nb_classes):
+def build_VGG19(img_h, img_w, nb_classes, channels=3):
     
-    INPUTSIZE = (img_w, img_h, 1)
+    INPUTSIZE = (img_h, img_w, channels)
     model = Sequential()
     
     model.add(Conv2D(64,(3,3),strides=(1,1),input_shape=INPUTSIZE,padding='same',activation='relu',kernel_initializer='random_normal'))  
@@ -145,6 +145,315 @@ def build_VGG19(img_w, img_h, nb_classes):
 ## model = build_VGG19(img_w, img_h, tone_num, channels=1)    
 # -------------------VGG19{E}-------------------
 
+
+# -------------------resnet50{S}-------------------
+
+
+def Conv2d_BN(x, nb_filter,kernel_size, strides=(1,1), padding='same',name=None):  
+    if name is not None:  
+        bn_name = name + '_bn'  
+        conv_name = name + '_conv'  
+    else:  
+        bn_name = None  
+        conv_name = None  
+  
+    x = Conv2D(nb_filter,kernel_size,padding=padding,strides=strides,activation='relu',name=conv_name)(x)  
+    x = BatchNormalization(axis=3,name=bn_name)(x)  
+    return x  
+  
+def Conv_Block(inpt,nb_filter,kernel_size,strides=(1,1), with_conv_shortcut=False):  
+    x = Conv2d_BN(inpt,nb_filter=nb_filter[0],kernel_size=(1,1),strides=strides,padding='same')  
+    x = Conv2d_BN(x, nb_filter=nb_filter[1], kernel_size=(3,3), padding='same')  
+    x = Conv2d_BN(x, nb_filter=nb_filter[2], kernel_size=(1,1), padding='same')  
+    if with_conv_shortcut:  
+        shortcut = Conv2d_BN(inpt,nb_filter=nb_filter[2],strides=strides,kernel_size=kernel_size)  
+        x = add([x,shortcut])  
+        return x  
+    else:  
+        x = add([x,inpt])  
+        return x  
+
+def resnet50(img_h, img_w, nb_classes, channels=3):
+
+	INPUTSIZE = (img_h, img_w, channels)
+
+    img_input = Input(shape=INPUTSIZE)  
+    x = ZeroPadding2D((3,3))(img_input)  
+    x = Conv2d_BN(x,nb_filter=64,kernel_size=(7,7),strides=(2,2),padding='valid')  
+    x = MaxPooling2D(pool_size=(3,3),strides=(2,2),padding='same')(x)  
+  
+    x = Conv_Block(x,nb_filter=[64,64,256],kernel_size=(3,3),strides=(1,1),with_conv_shortcut=True)  
+    x = Conv_Block(x,nb_filter=[64,64,256],kernel_size=(3,3))  
+    x = Conv_Block(x,nb_filter=[64,64,256],kernel_size=(3,3))  
+  
+    x = Conv_Block(x,nb_filter=[128,128,512],kernel_size=(3,3),strides=(2,2),with_conv_shortcut=True)  
+    x = Conv_Block(x,nb_filter=[128,128,512],kernel_size=(3,3))  
+    x = Conv_Block(x,nb_filter=[128,128,512],kernel_size=(3,3))  
+    x = Conv_Block(x,nb_filter=[128,128,512],kernel_size=(3,3))  
+  
+    x = Conv_Block(x,nb_filter=[256,256,1024],kernel_size=(3,3),strides=(2,2),with_conv_shortcut=True)  
+    x = Conv_Block(x,nb_filter=[256,256,1024],kernel_size=(3,3))  
+    x = Conv_Block(x,nb_filter=[256,256,1024],kernel_size=(3,3))  
+    x = Conv_Block(x,nb_filter=[256,256,1024],kernel_size=(3,3))  
+    x = Conv_Block(x,nb_filter=[256,256,1024],kernel_size=(3,3))  
+    x = Conv_Block(x,nb_filter=[256,256,1024],kernel_size=(3,3))  
+  
+    x = Conv_Block(x,nb_filter=[512,512,2048],kernel_size=(3,3),strides=(2,2),with_conv_shortcut=True)  
+    x = Conv_Block(x,nb_filter=[512,512,2048],kernel_size=(3,3))  
+    x = Conv_Block(x,nb_filter=[512,512,2048],kernel_size=(3,3))  
+    x = AveragePooling2D(pool_size=(7,7),padding='same')(x)  
+    x = Flatten()(x)
+    output = Dense(tone_num,activation='softmax')(x)
+
+    model=Model(input=img_input, output=output)
+    model.summary()
+
+    return model
+
+# -------------------resnet50{E}-------------------
+
+from keras import layers
+from keras.layers import GlobalMaxPooling2D
+
+def conv2d_bn(x,
+              filters,
+              num_row,
+              num_col,
+              padding='same',
+              strides=(1, 1),
+              name=None):
+    """Utility function to apply conv + BN.
+
+    # Arguments
+        x: input tensor.
+        filters: filters in `Conv2D`.
+        num_row: height of the convolution kernel.
+        num_col: width of the convolution kernel.
+        padding: padding mode in `Conv2D`.
+        strides: strides in `Conv2D`.
+        name: name of the ops; will become `name + '_conv'`
+            for the convolution and `name + '_bn'` for the
+            batch norm layer.
+
+    # Returns
+        Output tensor after applying `Conv2D` and `BatchNormalization`.
+    """
+    if name is not None:
+        bn_name = name + '_bn'
+        conv_name = name + '_conv'
+    else:
+        bn_name = None
+        conv_name = None
+    if K.image_data_format() == 'channels_first':
+        bn_axis = 1
+    else:
+        bn_axis = 3
+    x = Conv2D(
+        filters, (num_row, num_col),
+        strides=strides,
+        padding=padding,
+        use_bias=False,
+        name=conv_name)(x)
+    x = BatchNormalization(axis=bn_axis, scale=False, name=bn_name)(x)
+    x = Activation('relu', name=name)(x)
+    return x
+
+
+def inceptionv3(img_h, img_w, nb_classes, channels=3):
+    channel_axis = 3   # channels last
+
+    INPUTSIZE = (img_h, img_w, channels)
+    img_input = Input(shape=INPUTSIZE)  
+
+    x = conv2d_bn(img_input, 32, 3, 3, strides=(2, 2), padding='valid')
+    x = conv2d_bn(x, 32, 3, 3, padding='valid')
+    x = conv2d_bn(x, 64, 3, 3)
+    x = MaxPooling2D((3, 3), strides=(2, 2))(x)
+
+    x = conv2d_bn(x, 80, 1, 1, padding='valid')
+    x = conv2d_bn(x, 192, 3, 3, padding='valid')
+    x = MaxPooling2D((3, 3), strides=(2, 2))(x)
+
+    # mixed 0, 1, 2: 35 x 35 x 256
+    branch1x1 = conv2d_bn(x, 64, 1, 1)
+
+    branch5x5 = conv2d_bn(x, 48, 1, 1)
+    branch5x5 = conv2d_bn(branch5x5, 64, 5, 5)
+
+    branch3x3dbl = conv2d_bn(x, 64, 1, 1)
+    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+
+    branch_pool = AveragePooling2D((3, 3), strides=(1, 1), padding='same')(x)
+    branch_pool = conv2d_bn(branch_pool, 32, 1, 1)
+    x = layers.concatenate(
+        [branch1x1, branch5x5, branch3x3dbl, branch_pool],
+        axis=channel_axis,
+        name='mixed0')
+
+    # mixed 1: 35 x 35 x 256
+    branch1x1 = conv2d_bn(x, 64, 1, 1)
+
+    branch5x5 = conv2d_bn(x, 48, 1, 1)
+    branch5x5 = conv2d_bn(branch5x5, 64, 5, 5)
+
+    branch3x3dbl = conv2d_bn(x, 64, 1, 1)
+    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+
+    branch_pool = AveragePooling2D((3, 3), strides=(1, 1), padding='same')(x)
+    branch_pool = conv2d_bn(branch_pool, 64, 1, 1)
+    x = layers.concatenate(
+        [branch1x1, branch5x5, branch3x3dbl, branch_pool],
+        axis=channel_axis,
+        name='mixed1')
+
+    # mixed 2: 35 x 35 x 256
+    branch1x1 = conv2d_bn(x, 64, 1, 1)
+
+    branch5x5 = conv2d_bn(x, 48, 1, 1)
+    branch5x5 = conv2d_bn(branch5x5, 64, 5, 5)
+
+    branch3x3dbl = conv2d_bn(x, 64, 1, 1)
+    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+
+    branch_pool = AveragePooling2D((3, 3), strides=(1, 1), padding='same')(x)
+    branch_pool = conv2d_bn(branch_pool, 64, 1, 1)
+    x = layers.concatenate(
+        [branch1x1, branch5x5, branch3x3dbl, branch_pool],
+        axis=channel_axis,
+        name='mixed2')
+
+    # mixed 3: 17 x 17 x 768
+    branch3x3 = conv2d_bn(x, 384, 3, 3, strides=(2, 2), padding='valid')
+
+    branch3x3dbl = conv2d_bn(x, 64, 1, 1)
+    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+    branch3x3dbl = conv2d_bn(
+        branch3x3dbl, 96, 3, 3, strides=(2, 2), padding='valid')
+
+    branch_pool = MaxPooling2D((3, 3), strides=(2, 2))(x)
+    x = layers.concatenate(
+        [branch3x3, branch3x3dbl, branch_pool], axis=channel_axis, name='mixed3')
+
+    # mixed 4: 17 x 17 x 768
+    branch1x1 = conv2d_bn(x, 192, 1, 1)
+
+    branch7x7 = conv2d_bn(x, 128, 1, 1)
+    branch7x7 = conv2d_bn(branch7x7, 128, 1, 7)
+    branch7x7 = conv2d_bn(branch7x7, 192, 7, 1)
+
+    branch7x7dbl = conv2d_bn(x, 128, 1, 1)
+    branch7x7dbl = conv2d_bn(branch7x7dbl, 128, 7, 1)
+    branch7x7dbl = conv2d_bn(branch7x7dbl, 128, 1, 7)
+    branch7x7dbl = conv2d_bn(branch7x7dbl, 128, 7, 1)
+    branch7x7dbl = conv2d_bn(branch7x7dbl, 192, 1, 7)
+
+    branch_pool = AveragePooling2D((3, 3), strides=(1, 1), padding='same')(x)
+    branch_pool = conv2d_bn(branch_pool, 192, 1, 1)
+    x = layers.concatenate(
+        [branch1x1, branch7x7, branch7x7dbl, branch_pool],
+        axis=channel_axis,
+        name='mixed4')
+
+    # mixed 5, 6: 17 x 17 x 768
+    for i in range(2):
+        branch1x1 = conv2d_bn(x, 192, 1, 1)
+
+        branch7x7 = conv2d_bn(x, 160, 1, 1)
+        branch7x7 = conv2d_bn(branch7x7, 160, 1, 7)
+        branch7x7 = conv2d_bn(branch7x7, 192, 7, 1)
+
+        branch7x7dbl = conv2d_bn(x, 160, 1, 1)
+        branch7x7dbl = conv2d_bn(branch7x7dbl, 160, 7, 1)
+        branch7x7dbl = conv2d_bn(branch7x7dbl, 160, 1, 7)
+        branch7x7dbl = conv2d_bn(branch7x7dbl, 160, 7, 1)
+        branch7x7dbl = conv2d_bn(branch7x7dbl, 192, 1, 7)
+
+        branch_pool = AveragePooling2D(
+            (3, 3), strides=(1, 1), padding='same')(x)
+        branch_pool = conv2d_bn(branch_pool, 192, 1, 1)
+        x = layers.concatenate(
+            [branch1x1, branch7x7, branch7x7dbl, branch_pool],
+            axis=channel_axis,
+            name='mixed' + str(5 + i))
+
+    # mixed 7: 17 x 17 x 768
+    branch1x1 = conv2d_bn(x, 192, 1, 1)
+
+    branch7x7 = conv2d_bn(x, 192, 1, 1)
+    branch7x7 = conv2d_bn(branch7x7, 192, 1, 7)
+    branch7x7 = conv2d_bn(branch7x7, 192, 7, 1)
+
+    branch7x7dbl = conv2d_bn(x, 192, 1, 1)
+    branch7x7dbl = conv2d_bn(branch7x7dbl, 192, 7, 1)
+    branch7x7dbl = conv2d_bn(branch7x7dbl, 192, 1, 7)
+    branch7x7dbl = conv2d_bn(branch7x7dbl, 192, 7, 1)
+    branch7x7dbl = conv2d_bn(branch7x7dbl, 192, 1, 7)
+
+    branch_pool = AveragePooling2D((3, 3), strides=(1, 1), padding='same')(x)
+    branch_pool = conv2d_bn(branch_pool, 192, 1, 1)
+    x = layers.concatenate(
+        [branch1x1, branch7x7, branch7x7dbl, branch_pool],
+        axis=channel_axis,
+        name='mixed7')
+
+    # mixed 8: 8 x 8 x 1280
+    branch3x3 = conv2d_bn(x, 192, 1, 1)
+    branch3x3 = conv2d_bn(branch3x3, 320, 3, 3,
+                          strides=(2, 2), padding='valid')
+
+    branch7x7x3 = conv2d_bn(x, 192, 1, 1)
+    branch7x7x3 = conv2d_bn(branch7x7x3, 192, 1, 7)
+    branch7x7x3 = conv2d_bn(branch7x7x3, 192, 7, 1)
+    branch7x7x3 = conv2d_bn(
+        branch7x7x3, 192, 3, 3, strides=(2, 2), padding='valid')
+
+    branch_pool = MaxPooling2D((3, 3), strides=(2, 2))(x)
+    x = layers.concatenate(
+        [branch3x3, branch7x7x3, branch_pool], axis=channel_axis, name='mixed8')
+
+    # mixed 9: 8 x 8 x 2048
+    for i in range(2):
+        branch1x1 = conv2d_bn(x, 320, 1, 1)
+
+        branch3x3 = conv2d_bn(x, 384, 1, 1)
+        branch3x3_1 = conv2d_bn(branch3x3, 384, 1, 3)
+        branch3x3_2 = conv2d_bn(branch3x3, 384, 3, 1)
+        branch3x3 = layers.concatenate(
+            [branch3x3_1, branch3x3_2], axis=channel_axis, name='mixed9_' + str(i))
+
+        branch3x3dbl = conv2d_bn(x, 448, 1, 1)
+        branch3x3dbl = conv2d_bn(branch3x3dbl, 384, 3, 3)
+        branch3x3dbl_1 = conv2d_bn(branch3x3dbl, 384, 1, 3)
+        branch3x3dbl_2 = conv2d_bn(branch3x3dbl, 384, 3, 1)
+        branch3x3dbl = layers.concatenate(
+            [branch3x3dbl_1, branch3x3dbl_2], axis=channel_axis)
+
+        branch_pool = AveragePooling2D(
+            (3, 3), strides=(1, 1), padding='same')(x)
+        branch_pool = conv2d_bn(branch_pool, 192, 1, 1)
+        x = layers.concatenate(
+            [branch1x1, branch3x3, branch3x3dbl, branch_pool],
+            axis=channel_axis,
+            name='mixed' + str(9 + i))
+    if include_top:
+        # Classification block
+        x = GlobalAveragePooling2D(name='avg_pool')(x)
+        x = Dense(classes, activation='softmax', name='predictions')(x)
+    else:
+        if pooling == 'avg':
+            x = GlobalAveragePooling2D()(x)
+        elif pooling == 'max':
+            x = GlobalMaxPooling2D()(x)
+
+    inputs = img_input
+        
+    # Create model.
+    model = Model(inputs, x, name='inception_v3')
+
+    return model
 
 # -------------------inception_resnet_v1{S}-------------------
 
@@ -277,8 +586,8 @@ def reduction_resnet_B(x_input):
     return x
  
  
-def inception_resnet_v1_backbone(nb_classes=1000, scale=True):
-    x_input = Input(shape=(299, 299, 3))
+def inception_resnet_v1_backbone(img_h, img_w, nb_classes, channels=3,, scale=True):
+    x_input = Input(shape=(img_h, img_w, channels))
     # stem
     x = resnet_v1_stem(x_input)
  
@@ -444,10 +753,10 @@ def reduction_resnet_v2_B(x_input):
     return rbr
  
 ## min size: 300 * 300 * 1 
-def inception_resnet_v2(img_w, img_h, nb_classes, channels=3, scale=True):
+def inception_resnet_v2(img_h, img_w, nb_classes, channels=3, scale=True):
     '''Creates the Inception_ResNet_v1 network.'''
  
-    init = Input((img_w, img_h, channels))  # Channels last, as using Tensorflow backend with Tensorflow image dimension ordering
+    init = Input((img_h, img_w, channels))  # Channels last, as using Tensorflow backend with Tensorflow image dimension ordering
  
     # Input shape is 299 * 299 * 3
     x = resnet_v2_stem(init)  # Output: 35 * 35 * 256
@@ -687,3 +996,73 @@ def inception_v4(img_w, img_h, tone_num, channels=3):
 ## model = inceptionv4(img_w, img_h, tone_num, channels=1)
 
 # -------------------inceptionv4{E}-------------------    
+
+
+
+# include imagenet top, transfer learning build model
+# inceptionv3, vgg16, vgg19, resnet50, mobilenet, Xception, densenet121, densenet169, densenet201, inceptionresnetv2
+def build_model_options(application, img_h, img_w , num_class, channels=3):
+    ##shape can not smaller then
+    from keras.applications.inception_resnet_v2 import InceptionResNetV2
+    from keras.applications import densenet       ## densenet121, densenet169, densenet201
+    from keras.applications.inception_v3 import InceptionV3
+    from keras.applications.xception import Xception
+    from keras.applications.vgg16 import VGG16
+    from keras.applications.vgg19 import VGG19
+    from keras.applications.resnet50 import ResNet50
+    from keras.applications.mobilenet import MobileNet
+    from keras.applications.nasnet import NASNet  ## NASNetLarge, NASNetMobile
+    
+    INPUTSIZE = (img_h, img=w, channels)
+
+    # min size: 139 * 139 * 1  => voice mfcc  
+    if application == 'inceptionv3':
+        base_model = InceptionV3(include_top=False, weights=None,
+            input_shape=INPUTSIZE, pooling='max')          
+    # smallest: 48 * 48 default: 224 * 224    
+    elif application == 'vgg16':
+        base_model = VGG16(include_top=False, weights=None,
+            input_shape=INPUTSIZE, pooling='max')  
+    elif application == 'vgg19':
+        base_model = VGG19(include_top=False, weights=None,
+            input_shape=INPUTSIZE, pooling='max')  
+    # smallest: 197 * 197 default: 224 * 224      
+    elif application == 'resnet50':
+        base_model = ResNet50(include_top=False, weights=None,
+            input_shape=INPUTSIZE, pooling='max')  
+    # smallest: 32 * 32 default: 224 * 224    
+    elif application == 'mobilenet':
+        base_model = MobileNet(include_top=False, weights=None,
+            input_shape=INPUTSIZE, pooling='max')  
+    elif application == 'Xception':
+        base_model = Xception(include_top=False, weights=None,
+            input_shape=INPUTSIZE, pooling='max')  
+    elif application == 'densenet121':
+        base_model = densenet.DenseNet121(include_top=False, weights=None,
+            input_shape=INPUTSIZE, pooling='max')  
+    elif application == 'densenet169':
+        base_model = densenet.DenseNet169(include_top=False, weights=None,
+            input_shape=INPUTSIZE, pooling='max')
+    # min size: 221 * 221 * 1  => voice mfcc
+    elif application == 'densenet201':       
+        print('densenet201')
+        base_model = densenet.DenseNet201(include_top=False, weights=None,
+            input_shape=INPUTSIZE, pooling='max')        
+    elif application == 'nasnetlarge':
+        base_model = densenet.NASNetLarge(include_top=False, weights=None,
+            input_shape=INPUTSIZE, pooling='max')
+    elif application == 'nasnetmobile':
+        base_model = densenet.NASNetMobile(include_top=False, weights=None,
+            input_shape=INPUTSIZE, pooling='max')
+    elif application == 'inceptionresnetv2':
+        base_model = InceptionResNetV2(include_top=False, weights=None,
+            input_shape=INPUTSIZE, pooling='max')
+    
+    x = base_model.output
+    #x = Dense(lastdensesize, activation='relu')(x)
+    # 最後一層不要 batch normalize / dropout
+    outputs = Dense(num_class, activation='softmax')(x)    
+    model = Model(base_model.inputs, outputs)
+    #model.summary()
+    
+    return model
